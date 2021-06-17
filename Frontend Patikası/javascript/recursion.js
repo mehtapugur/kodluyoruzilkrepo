c4545ef95da70957fb27e996bb41354ba8f7c199
Mehtap Uğur <yaziliminval@hotmail.com>
2021-06-18 00:17:41 +0300

    Update recursion.js

--- Frontend Patikası/javascript/recursion.js
+++ Frontend Patikası/javascript/recursion.js
@@ -25,3 +25,14 @@ function pow(x, n) {
 }
 
 alert(pow(2, 3)); // 8
+
+//pow(2,4) = 2 * pow(2,3)
+//pow(2,3) = 2 * pow(2,2)
+//pow(2,2) = 2 * pow(2,1)
+//pow(2,1) = 2
+//Sonuç olarak recursion, bir fonksiyonu en basit haline kadar getiriyor. Sonuç en açık hale gelene kadar.
+
+//Aşağıdaki soruyu recursion ile arrow function ve if else clause kullanarak yeniden nasıl yazarız
+function pow(x, n) {
+  return n == 1 ? x : x * pow(x, n - 1);
+}
