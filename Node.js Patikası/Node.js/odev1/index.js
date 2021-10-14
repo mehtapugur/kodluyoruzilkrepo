af78e59b57e017b8ad6b07363ca67cf87b65f946
Mehtap Uğur <yaziliminval@hotmail.com>
2021-10-14 19:02:15 +0300

    Delete index.js

--- Node.js Patikası/Node.js/odev1/index.js
+++
@@ -1,9 +0,0 @@
-const arguments = process.argv.slice(2);
-
-function findArea(perimeter) {
-  
-  const area = Math.PI * perimeter**2;
-  console.log(`Yarıcapı ${perimeter} olan dairenin alanı: ${area}`);
-}
-
-findArea(arguments[0]*1);
